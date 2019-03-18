bdb8675a22b89380e8e7c9052de33dd6c642c581
anil <anil.kopparthy@gmail.com>
2019-03-17 23:32:06 -0400

    Initial Commit

---
+++ Replica/src/main/java/com/replica/ReplicaApplication.java
@@ -0,0 +1,13 @@
+package com.replica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplicaApplication.class, args);
+	}
+
+}
